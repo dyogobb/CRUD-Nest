a44ebe3664eb26bada630335ae9878afa422d264
dyogobb <dyogobbbj@gmail.com>
2024-09-19 12:42:17 -0300

    Removendo alguns arquivos inúteis

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,17 +1,7 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller('getHello')
 export class AppController {
   constructor(private readonly appService: AppService) {}
-
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
-  }
-
-  @Get(':id/:classe')
-  parametro(@Param('id') id: any, @Param('classe') classe: string): string {
-    return `Exibindo informações do id: ${id} de classe ${classe}`;
-  }
 }
